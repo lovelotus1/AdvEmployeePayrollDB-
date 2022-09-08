20f497358d67382f3dfafe919357c8a430a9aa34
“Love <opera.prem@gmail.com>
2022-09-08 22:07:11 +0530

    Added DataBaseConnection

---
+++ EmployeeManagement/EmployeeManagement/EmployeeRepo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement
+{
+    public class EmployeeRepo
+    {
+        public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EmployeePayrollService;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+    }
+}
